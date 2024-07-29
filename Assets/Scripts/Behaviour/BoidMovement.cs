f5b19f680b5d57d12fa9df31fe548479466aceaf
Ddraigo <laithanh1000@gmail.com>
2024-07-29 17:15:38 +0700

    Update with SphereCast and OverlapSphere

--- Assets/Scripts/Behaviour/BoidMovement.cs
+++ Assets/Scripts/Behaviour/BoidMovement.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BoidMovement : MonoBehaviour
 {
     
     public Vector3 Velocity { get; private set; }
-    public float visionAngle = 270f;
+    public float visionAngle;
     public float radius;
     public float forwardSpeed;
     public float turnSpeed;
@@ -18,6 +19,11 @@ public class BoidMovement : MonoBehaviour
     [SerializeField] private float rateOfAligment;
     [SerializeField] private float rateOfCohesion;
     [SerializeField] private Boundery boundery;
+    [SerializeField] private LayerMask boidLayerMask;
+
+    private List<BoidMovement> boidsInRange = new List<BoidMovement>();
+    private List<BoidMovement> boidsToAvoid = new List<BoidMovement>();
+
 
     private void FixedUpdate()
     {
@@ -25,11 +31,15 @@ public class BoidMovement : MonoBehaviour
         LimitBoundery();
         transform.position += Velocity * Time.fixedDeltaTime;
         LookRotation();
+        Debug.Log(boidsInRange.Count);
+        Debug.Log("AVOID: " + boidsToAvoid.Count);
     }
+
+   
 
     private Vector2 CalculateVelocity()
     {
-        var boidInRange = BoidsInRange();
+        var boidInRange = FindBoidsInRange();
         var boidToAvoid = ProtectedArea();
         Vector2 velocity = ((Vector2)transform.forward + rateOfSeparation * Separation(boidToAvoid)
                                                        + rateOfAligment * Aligment(boidInRange) 
@@ -45,13 +55,14 @@ public class BoidMovement : MonoBehaviour
         if (Mathf.Abs(position.x) > boundery.XLimit)
         {
             direction.x = - direction.x;
+            
         }
 
         if (Mathf.Abs(position.y) > boundery.YLimit)
         {
             direction.y = - direction.y;
         }
-
+        
         Velocity = direction * Velocity.magnitude;
     }
 
@@ -59,31 +70,111 @@ public class BoidMovement : MonoBehaviour
     {
         transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(Velocity), turnSpeed * Time.fixedDeltaTime);
     }
-    
+
     // list cac boid nanm trong pham vi anh huong
-    private List<BoidMovement> BoidsInRange()
+    //private List<BoidMovement> FindBoidsInRange()
+    //{
+    //    boidsInRange.Clear();
+    //    Collider[] colliders = Physics.OverlapSphere(transform.position, radius, boidLayerMask);
+
+    //    foreach (var collider in colliders)
+    //    {
+    //        BoidMovement boid = collider.GetComponent<BoidMovement>();
+    //        if (boid != null && boid != this && InVisionCone(boid.transform.position))
+    //        {
+    //            boidsInRange.Add(boid);
+    //        }
+    //    }
+
+    //    return boidsInRange;
+    //}
+
+    private List<BoidMovement> FindBoidsInRange()
     {
-        var listBoid = boids.boidMovements.FindAll(boid => boid != this
-            && (boid.transform.position - transform.position).magnitude <= radius
-            && InVisionCone(boid.transform.position));
-        return listBoid;
+        boidsInRange.Clear();
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius, boidLayerMask);
+
+        foreach (var collider in colliders)
+        {
+            BoidMovement boid = collider.GetComponent<BoidMovement>();
+            if (boid != null )
+            {
+                boidsInRange.Add(boid);
+            }
+        }
+
+        return boidsInRange;
     }
 
     private List<BoidMovement> ProtectedArea()
     {
-        var listBoid = boids.boidMovements.FindAll(boid => boid != this
-            && (boid.transform.position - transform.position).magnitude <= ratio
-            && InVisionCone(boid.transform.position));
-        return listBoid;
+        boidsToAvoid.Clear();
+
+        foreach (var boid in boidsInRange)
+        {
+            if (boid != null && (boid.transform.position - transform.position).magnitude <= ratio)
+            {
+                boidsToAvoid.Add(boid);
+            }
+        }
+
+        return boidsToAvoid;
     }
 
-    private bool InVisionCone(Vector2 position)
-    {
-        Vector2 directionToPosition = position - (Vector2)transform.position;
-        float dotProduct = Vector2.Dot(transform.forward, directionToPosition);
-        float cosHalfVisonAngle = Mathf.Cos(visionAngle * 0.5f * Mathf.Deg2Rad);
-        return dotProduct >= cosHalfVisonAngle;
-    }
+    //private List<BoidMovement> FindBoidsInRange()
+    //{
+    //    if(boidsInRange.Count > 0)
+    //        boidsInRange.Clear();
+    //    float angleStep = visionAngle / (15 - 1);
+
+    //    for (int i = 0; i < 15; i++)
+    //    {
+    //        float angle = -visionAngle / 2 + i * angleStep;
+    //        Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+    //        if (Physics.SphereCast(transform.position, radius, direction, out RaycastHit hit, radius, boidLayerMask))
+    //        {
+    //            BoidMovement boid = hit.collider.GetComponent<BoidMovement>();
+    //            if (boid != null && boid != this)
+    //            {
+    //                boidsInRange.Add(boid);
+    //            }
+    //        }
+    //    }
+    //    return boidsInRange;
+    //}
+
+
+    //private List<BoidMovement> ProtectedArea()
+    //{
+    //    if (boidsToAvoid.Count > 0)
+    //        boidsToAvoid.Clear();
+    //    float angleStep = visionAngle / (15 - 1);
+
+    //    for (int i = 0; i < 15; i++)
+    //    {
+    //        float angle = -visionAngle / 2 + i * angleStep;
+    //        Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+    //        if (Physics.SphereCast(transform.position, ratio, direction, out RaycastHit hit, ratio, boidLayerMask))
+    //        {
+    //            BoidMovement boid = hit.collider.GetComponent<BoidMovement>();
+    //            if (boid != null && boid != this)
+    //            {
+    //                boidsToAvoid.Add(boid);
+    //            }
+    //        }
+    //    }
+    //    return boidsToAvoid;
+    //}
+
+    //private bool InVisionCone(Vector2 position)
+    //{
+    //    Vector2 directionToPosition = position - (Vector2)transform.position;
+    //    float dotProduct = Vector2.Dot(transform.forward, directionToPosition);
+    //    float cosHalfVisonAngle = Mathf.Cos(visionAngle * 0.5f * Mathf.Deg2Rad);
+    //    return dotProduct >= cosHalfVisonAngle;
+    //}
 
 
     private void OnDrawGizmosSelected()
@@ -93,12 +184,18 @@ public class BoidMovement : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, ratio);
 
-        var boidsRange = BoidsInRange();
-        foreach (var boid in boidsRange)
+        //float angleStep = visionAngle / (15 - 1);
+        //for (int i = 0; i < 15; i++)
+        //{
+        //    float angle = -visionAngle / 2 + i * angleStep;
+        //    Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+        //    Gizmos.DrawLine(transform.position, transform.position + direction * radius);
+        //}
+
+        foreach (var boid in FindBoidsInRange())
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawLine(transform.position, boid.transform.position);
-
         }
     }
 
@@ -121,45 +218,27 @@ public class BoidMovement : MonoBehaviour
 
     private Vector2 Aligment(List<BoidMovement> boidMovements)
     {
-        Vector2 direction = Vector2.zero; ;
-        
+        Vector3 direction = Vector3.zero;
+
         foreach (var boid in boidMovements)
         {
-            direction += (Vector2)boid.Velocity;
+            direction += boid.Velocity;
         }
 
-        if (boidMovements.Count != 0) 
-        {
-            // tinh trung binh van toc 
-            direction /= boidMovements.Count;
-        }
-        else
-        {
-            direction = Velocity;
-        }
-        return direction.normalized;
+        return boidMovements.Count > 0 ? (direction / boidMovements.Count).normalized : transform.forward;
     }
 
     private Vector2 Cohesion(List<BoidMovement> boidMovements)
+{
+    Vector3 center = Vector3.zero;
+
+    foreach (var boid in boidMovements)
     {
-        Vector2 direction;
-        Vector2 center = Vector2.zero;
-        foreach (var boid in boidMovements)
-        {
-            center += (Vector2)boid.transform.position;
-        }
-
-        if (boidMovements.Count != 0)
-        {
-            center /= boidMovements.Count;
-        }
-        else
-        {
-            center = transform.position;
-        }
-
-        direction = center - (Vector2)transform.position; // tinh vector tu vi tri hien tai den boid trung tam 
-        return direction.normalized;
+        center += boid.transform.position;
     }
+
+    center = boidMovements.Count > 0 ? center / boidMovements.Count : transform.position;
+    return (center - transform.position).normalized;
+}
    
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] private ListBoidVariable boids;
+    //[SerializeField] private ListBoidVariable boids;
     [SerializeField] private GameObject boidsPrefab;
     [SerializeField] private float boidCount;
 
     private void Start()
     {
-        if(boids.boidMovements.Count > 0) 
-            boids.boidMovements.Clear();  // chuan bi cho viec tao moi
-
+        //if(boids.boidMovements.Count > 0) 
+        //    boids.boidMovements.Clear();  // chuan bi cho viec tao moi
+        boidCount = Random.Range(60, 100);
         for (int i = 0; i  < boidCount; i++)
         {
             float direction = Random.Range(0f, 360f); // huong ngau nhien
@@ -21,8 +21,8 @@ public class SpawnManager : MonoBehaviour
             // Sinh ra moi boid co vi tri va huong ngau nhien
             GameObject boid = Instantiate(boidsPrefab, position, Quaternion.Euler( Vector3.forward * direction) * boidsPrefab.transform.localRotation); 
             boid.transform.SetParent(transform);
-            boids.boidMovements.Add(boid.GetComponent<BoidMovement>()); 
-
+            //boids.boidMovements.Add(boid.GetComponent<BoidMovement>()); 
+            
             transform.position = position;
         }
     }
